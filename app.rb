74040a8cfb0c55d672651fd41db9f82bf8f6f205
Annie Shin <annieyshin@gmail.com>
2018-07-20 11:31:20 -0700

    add in more view content, set up class for each variable

---
+++ app.rb
@@ -0,0 +1,6 @@
+require("bundler/setup")
+require("sinatra/activerecord")
+Bundler.require(:default)
+require('pry')
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
---
+++ lib/brand_store.rb
@@ -0,0 +1,4 @@
+class BrandStore < ActiveRecord::Base
+  belongs_to :brand
+  belongs_to :store
+end
