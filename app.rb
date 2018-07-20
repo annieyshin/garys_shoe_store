d396866f9c02abc0503c2cc799796ee23ffb332c
Annie Shin <annieyshin@gmail.com>
2018-07-20 14:16:27 -0700

    add page content and get posts for app.rb

--- app.rb
+++ app.rb
@@ -4,3 +4,17 @@ Bundler.require(:default)
 require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+get('/stores') do
+  @stores = Store.all
+  erb(:stores)
+end
+
+get('/brands') do
+  @brands = Brand.all
+  erb(:brands)
+end
